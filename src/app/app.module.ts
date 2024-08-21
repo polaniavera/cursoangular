dbb306e7a9b36c9ec208ef2fcf562bcd98f74108
Cristhian Polania <polaniavera@gmail.com>
2024-08-21 12:12:53 +0300

    day2.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
     AppRoutingModule
   ],
   providers: [],
